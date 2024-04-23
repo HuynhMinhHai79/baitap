03a9cbcb8ac649ad6ce03b6aa954ffd226f6dcc7
armeiitt <148439942+armeiitt@users.noreply.github.com>
2024-04-23 20:58:07 +0700

    my

---
+++ src/baitap/PropertyOwner.java
@@ -0,0 +1,42 @@
+package baitap;
+
+import java.util.ArrayList;
+
+public class PropertyOwner extends user {
+
+    public PropertyOwner(int id, String name, String email, String phoneNumber, String password) {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+    private ArrayList<Property> ListProperty;
+
+    public PropertyOwner(int id, String name, String email, String phoneNumber, String password,
+            ArrayList<Property> listProperty) {
+        super();
+        ListProperty = listProperty;
+    }
+
+    public ArrayList<Property> getListProperty() {
+        return ListProperty;
+    }
+
+    public void setListProperty(ArrayList<Property> listProperty) {
+        ListProperty = listProperty;
+    }
+
+    public void listProperties() {
+        System.out.println("Owned Properties:");
+        for (Property property : ListProperty) {
+            System.out.println(property.toString());
+        }
+    }
+
+    public void addProperty(Property property) {
+        ListProperty.add(property);
+    }
+
+    public void removeAccount(Property property) {
+        ListProperty.remove(property);
+    }
+}
---
+++ src/baitap/TenantBuilder.java
@@ -0,0 +1,12 @@
+package baitap;
+
+import java.util.ArrayList;
+
+public class TenantBuilder {
+
+    public ArrayList<RentalContract> getList() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getList'");
+    }
+
+}
